c415c39f2ae8c348aa40feaeacb3f5160ff1f70c
lankybox02 <104015308+lankybox02@users.noreply.github.com>
2022-04-26 22:47:05 +0000

    Fix #1

--- pagetypes.js
+++ pagetypes.js
@@ -17,17 +17,38 @@ let pageTypes = {
   },
   "explore": {
     "title": "Explore",
-    "content": "<h1>Explore</h1>The post API hasn't been implemented over this page yet.",
+    "content": `<h1>Explore</h1><div style="text-align:left" id="tiles"><span class="header">Featured Users</span><br><span>Failed to reach API</span></div>`,
     "accountonly": false
   },
   "settings": {
     "title": "Settings",
-    "content": "<h1>Settings</h1>The account API hasn't been implemented over this page yet.",
+    "content": "<h1>Settings</h1><span>Failed to reach API</span>",
     "accountonly": true
   },
   "messages": {
     "title": "Messages",
-    "content": "<h1>Settings</h1>The account API hasn't been implemented over this page yet.",
+    "content": "<h1>Settings</h1><span>Failed to reach API</span>",
     "accountonly": true
+  },
+  "signup": {
+    "title": "Sign Up",
+    "content": `<div style="display: flex;text-align:left;">
+    <img src="assets/hearts.png" width="300px">
+    <div style="margin-left: 20px;">
+    <span class="header">Sign up for Riverbox!</span>
+    <span style="margin-top: 15px;display: block;">
+    RiverBox: The simple &amp; friendly community to discuss absolutely anything.
+    </span>
+
+    <input placeholder="Insert a username..." id="usernameInput" />
+    <button style="margin-top: 10px;" onclick="signUp()">
+      Sign up!
+    </button>
+    <br><br>
+    <span style="font-weight:bold">Username requirements:</span>
+    <ul><li>A username can only contain letters (a-z, A-Z), dash (-) and underscore (_)</li><li>A username can only be between 3 to 20 characters long</li><li>A username cannot contain the word "Admin", "Hacker" or "Bot"</li></ul>
+    </div>
+</div>`,
+    "accountonly": false
   }
 }
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let lastPageVisited;
 let loadTimeout;
+let loggedOut;
 
 if (localStorage.getItem("username") != null && localStorage.getItem("session") != null) {
 dispatchLoadingScreen();
@@ -18,14 +19,27 @@ function computeLoginData(data) {
     }else{
     insertNav("$")
     alert("Error: " + data.error)
+    localStorage.removeItem("session");
     }
     dispatchPageLoad("home")
 }
 
 function insertNav(username) {
-let styling;
+let accountControls;
+accountControls = `
+<div id="accountControls">
+<span onclick="dispatchPageLoad('settings')">Settings</span>
+<span onclick="dispatchPageLoad('messages')">Messages</span>
+<span onclick="dispatchPageLoad('${username}')">${username}</span>
+</div>
+`;
 if (username == "$") {
-    styling = `style="display:none"`;
+    loggedOut = true;
+    accountControls = `
+<div id="accountControls">
+<span onclick="dispatchPageLoad('signup')">Sign Up</span>
+</div>
+`;
 }
 document.getElementById("navbar").innerHTML = `
 <div class="homeNavButtons" onclick="dispatchPageLoad('home')">
@@ -34,11 +48,7 @@ RiverBox
 </div>
 <span onclick="dispatchPageLoad('explore')">Explore</span>
 <span onclick="dispatchPageLoad('about')">About</span>
-<div id="accountControls" ${styling}>
-<span onclick="dispatchPageLoad('settings')">Settings</span>
-<span onclick="dispatchPageLoad('messages')">Messages</span>
-<span onclick="dispatchPageLoad('${username}')">${username}</span>
-</div>
+${accountControls}
 `;
 }
 
@@ -49,11 +59,17 @@ function dispatchLoadingScreen(alwayswait) {
 function dispatchPageLoad(pageType) {
   dispatchLoadingScreen();
   lastPageVisited = pageType;
-  if (pageTypes[pageType] != null && pageTypes[pageType].accountonly == false) {
+  if (pageTypes[pageType] != null) {
+    if (pageTypes[pageType].accountonly == true) {
+      document.title = "Whoops! - RiverBox";
+    document.getElementById("pageContent").innerHTML = `Account-only pages aren't currently supported!`;
+    clearTimeout(loadTimeout)
+  }else{
     document.title = pageTypes[pageType].title + " - RiverBox";
     document.getElementById("pageContent").innerHTML = pageTypes[pageType].content;
     clearTimeout(loadTimeout)
   }
+    }
   loadTimeout = setTimeout(function(){
     if (document.getElementById("pageContent").innerHTML.includes("Give us a moment...</span>")) {
       document.getElementById("pageContent").innerHTML = `<span class="header">Well, this is embarrassing...</span><br><span>It appears that the page you tried to visit isn't loading correctly.<br>Click the re-try button, and if it doesn't work, please reload the page.</span><br><button onclick="dispatchPageLoad('${pageType}')">Re-try</button>`;
@@ -79,4 +95,52 @@ document.getElementById("pageContent").insertAdjacentHTML("beforeEnd", `<div sty
 <br>
 <div style="float: right;color: grey;">By <b style="color: black;">` + x[data].author + `</b> on <span style="color: black;">` + x[data].timestamp + `</span></div></div><br><br>`);
 }
+}
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+const signUp = () => {
+  let usernameInput = document.getElementById("usernameInput").value;
+  if(usernameInput.includes("/") || usernameInput.includes("#") || usernameInput.includes("?") || usernameInput.includes("%")) {
+    document.getElementById("usernameInput").value = "";
+    alert("Cannot encode username correctly!")
+  }else{
+postData('https://riverbox-api.lankybox02.repl.co/signup', JSON.parse(`{"username": "${usernameInput}"}`))
+  .then(data => {
+    logIn(data);
+  });
+  }
+}
+
+function logIn(data) {
+  if (data.success == false) {
+    alert("Error: " + data.error)
+  }else{
+    localStorage.setItem("username", data.username);
+    localStorage.setItem("session", data.session);
+  }
+}
+
+function logOut() {
+  if (confirm("Are you sure you want to log out? You will not be able to log back in!")) {
+    localStorage.removeItem("session");
+    window.location.reload();
+  }
 }
