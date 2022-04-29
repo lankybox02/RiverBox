ff5a4a6688576c29b09ef6359a963e0fae55c29e
Lanky2 <104015308+lankybox02@users.noreply.github.com>
2022-04-29 19:42:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let logged;
 let admin;
 
 version = "Public Beta 1.1";
+document.getElementById("version").innerText = version;
 
 fetch("https://riverbox-api.lankybox02.repl.co/")
    .then(response => response.json())
@@ -44,6 +45,7 @@ function computeLoginData(data) {
     logged = false;
     }
     dispatchPageLoad("home")
+    loadAdminTools();
 }
 
 function insertNav(username) {
@@ -51,7 +53,7 @@ let accountControls;
 accountControls = `
 <div id="accountControls">
 <span onclick="dispatchPageLoad('settings')">Settings</span>
-<span onclick="dispatchPageLoad('${username}')">${username}</span>
+<span onclick="viewUserPage('${username}')">${username}</span>
 </div>
 `;
 if (username == "$") {
@@ -67,8 +69,8 @@ document.getElementById("navbar").innerHTML = `
 <img src="assets/logo.png">
 RiverBox
 </div>
-<span onclick="modal('oops')">Explore</span>
-<span onclick="modal('oops')">About</span>
+<span onclick="modal('exploreholup')">Explore</span>
+<span onclick="modal('aboutwait')">About</span>
 ${accountControls}
 `;
 }
@@ -122,8 +124,8 @@ var postKeys = Object.keys(x);
 
 for(let i = postKeys.length; i > 0; i--) {
 data = postKeys[i - 1];
-document.getElementById("pageContent").insertAdjacentHTML("beforeEnd", `<div class="post">` + x[data].author + `<span style="margin-top:7px;margin-bottom:7px;display:block">` + convertPost(x[data].content) + `</span>
-<span onclick="likePost(this)" class="likeButton" ` + loggedLike() + `>Like (${x[data].likes})</span> <span class="hiddenModButton" onclick="modPost(this)" ` + adminClassLoad() + `>(Moderate)</span>
+document.getElementById("pageContent").insertAdjacentHTML("beforeEnd", `<div class="post"><span style="color: lightgrey;">` + x[data].author + `</span><span style="margin-top:7px;margin-bottom:7px;display:block">` + convertPost(x[data].content) + `</span>
+<span onclick="likePost(this, ${i})" class="likeButton" ` + loggedLike() + `>Like (${x[data].likes})</span> <span class="hiddenModButton" onclick="modPost(${i})" ` + adminClassLoad() + `>(Moderate)</span>
 <div style="float: right;color: lightgrey;">` + humanized_time_span(x[data].timestamp) + `</div></div><br><br>`);
 }
 }
@@ -143,7 +145,11 @@ function adminClassLoad(){
 }
 
 function convertPost(postContent) {
-  return atob(postContent.split("<")[0]).replaceAll("[b]", `<b>`).replaceAll("[/b]", `</b>`).replaceAll("[i]", `<i>`).replaceAll("[/i]", `</i>`).replaceAll("[u]", `<u>`).replaceAll("[/u]", `</u>`).replaceAll("[s]", `<s>`).replaceAll("[/s]", `</s>`);
+  if (postContent == "<span style='color:red;font-style:italic'>(This post was moderated)</span>") {
+    return postContent;
+  }else{
+    return atob(postContent).replaceAll("[b]", `<b>`).replaceAll("[/b]", `</b>`).replaceAll("[i]", `<i>`).replaceAll("[/i]", `</i>`).replaceAll("[u]", `<u>`).replaceAll("[/u]", `</u>`).replaceAll("[s]", `<s>`).replaceAll("[/s]", `</s>`);
+  }
 }
 
 // Example POST method implementation:
@@ -226,23 +232,54 @@ function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
 
-function modPost(e) {
-let postId = e.parentElement.children[0].children[0].innerText;
+function modPost(postId) {
 postData('https://riverbox-api.lankybox02.repl.co/remove', JSON.parse(`{"username": "` + localStorage.getItem("username") + `", "session": "` + localStorage.getItem("session") + `", "post": "` + postId + `"}`))
   .then(data => {
     window.location.reload();
   });
 }
 
-function likePost(e) {
-let postId = e.parentElement.children[0].children[0].innerText;
+function likePost(e, postId) {
+let before = e.innerText.replace(/\D/g, "");
+let newCount = parseInt(before) + 1;
+e.innerText = "Liked (...)";
+e.style.fontDecoration = null;
+e.style.cursor = null;
 postData('https://riverbox-api.lankybox02.repl.co/like', JSON.parse(`{"username": "` + localStorage.getItem("username") + `", "session": "` + localStorage.getItem("session") + `", "post": "${postId}"}`))
   .then(data => {
     if (data.success) {
-      let newCount = parseInt(e.innerText.replace(/\D/g, "")) + 1;
       e.innerText = "Liked (" + newCount.toString() + ")";
-      e.style.fontDecoration = null;
-      e.style.cursor = null;
+    }else{
+      e.innerText = "Liked (" + before + ")";
     }
   });
+}
+
+function loadAdminTools() {
+  if (admin) {
+    document.getElementById("navbar").insertAdjacentHTML("afterEnd", `<div style="background-color: #222330;display: inline-block;padding: 10px;border-bottom-left-radius: 20px;position: fixed;right: 0;"><input placeholder="Modal name..." id="modalinput"><br><input placeholder="Page name..." id="pageinput"></div>`);
+    document.getElementById("modalinput").addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        modal(document.getElementById("modalinput").value);
+      }
+    });
+    
+    document.getElementById("pageinput").addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        dispatchPageLoad(document.getElementById("pageinput").value);
+      }
+    });
+  }
+}
+
+function exploreLoad() {
+  for (let i = featuredUsers.length - 1;i > -1;i--) {
+    document.getElementById("tiles").insertAdjacentHTML("beforeEnd", `<div class="explore-tile" onclick="viewUserPage('${featuredUsers[i]}')"><span>` + featuredUsers[i] + `</span></div>`)
+  }
+}
+
+function viewUserPage() {
+  modal('betauseralert');
 }
